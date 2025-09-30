9c6b6792986a87341ac200921cc7c16a3fc11931
Ananya54321 <ananyapappula521@gmail.com>
2025-09-30 13:00:08 +0530

    auth service initialized

---
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -0,0 +1,18 @@
+// register a new user
+
+import { Request, Response, NextFunction } from "express";
+import { checkOtpRestrictions, validateRegistrationData } from "../utils/auth.helper";
+import prisma from "../../../../packages/libs/prisma";
+import { ValidationError } from "../../../../packages/error-handler";
+
+export const userRegistration = async (req : Request, res : Response, next : NextFunction) => {
+    validateRegistrationData(req.body, "user");
+    const {name, email} = req.body;
+    const existingUser = await prisma.user.findUnique({where : email});
+    if(existingUser){
+        return next(new ValidationError("User already exists with this email"));
+    };
+
+    await checkOtpRestrictions(email, next);
+
+}
---
+++ apps/auth-service/src/main.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import { errorMiddleware } from '../../../packages/error-handler/error-middleware';
+import cookieParser from 'cookie-parser';
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
+
+const app = express();
+
+app.use(
+  cors({
+    origin : ['http://localhost:3000'],
+    allowedHeaders : ['Authorization', 'Content-Type'],
+    credentials : true,
+  })
+)
+
+app.use(express.json());
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello API' });
+});
+
+app.use(errorMiddleware);
+
+const server = app.listen(port, ()=> { 
+  console.log(`Auth service running at http://${host}:${port}/api`);
+})
+
+
+// error logging
+server.on('error', (err)=>{
+  console.log("Server Error : ",err);
+})
