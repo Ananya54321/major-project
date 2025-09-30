18adc5c820907be120fc4cf6bc5cc5dc825824fb
Ananya54321 <ananyapappula521@gmail.com>
2025-09-30 18:54:59 +0530

    added otp with email template

--- apps/auth-service/src/utils/auth.helper.ts
+++ apps/auth-service/src/utils/auth.helper.ts
@@ -1,6 +1,8 @@
 import crypto from 'crypto';
 import { ValidationError } from '../../../../packages/error-handler';
 import redis from '../../../../packages/libs/redis';
+import { sendEmail } from './send-mail';
+import { NextFunction } from 'express';
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -15,12 +17,31 @@ export const validateRegistrationData = (data : any, userType : "user" | "seller
     }
 }
 
-export const checkOtpRestrictions = (email : string, next : NewableFunction) => {
-
+export const checkOtpRestrictions = async (email : string, next : NextFunction) => {
+    if(await redis.get(`otp_lock:${email}`)){
+        return next(new ValidationError("Account locked due to multiple failed attempts. Try again after 30 min"));
+    
+    }
+    if(await redis.get(`otp_spam_lock:${email}`)){
+        return next(new ValidationError("Too many OTP requests! Please wait 1 hour before requesting again"));
+    }
+    if(await redis.get(`otp_cooldown:${email}`)){
+        return next(new ValidationError("Please wait for 1 minute before requesting a new OTP!"));
+    }
 }
 export const sendOtp = async (name : string, email : string, template : string) => {
     const otp = crypto.randomInt(1000, 9999).toString();
-    await sendEmail();
+    await sendEmail(email, "Verify your email", template, {name, otp});
     await redis.set(`otp:${email}`, otp, "EX", 300);
     await redis.set(`otp_cooldown:${email}`, "true", "EX", 60);
+}
+
+export const trackOtpRequests = async (email : string, next : NextFunction) => {
+    const otpRequestKey = `otp_request_count:${email}`;
+    let otpRequests = parseInt((await redis.get(otpRequestKey)) || "0");
+    if(otpRequests >= 2){
+        await redis.set(`otp_spam_lock:${email}`, "locked", "EX", 3600); // locks for 1 hour, time is taken in seconds, i.e., 3600 seconds
+        return next(new ValidationError("Too many OTP requests placed. Please wait 1 hour before requesting again."));
+    }
+    await redis.set(otpRequestKey, otpRequests + 1, "EX", 3600);
 }
